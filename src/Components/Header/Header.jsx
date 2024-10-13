db6b290c61f97d1b52164c882be1db91e2173338
Sanjay-adepu <adepusanjay812@gmail.com>
2024-10-13 14:41:47 +0530

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header=()=>{
   
   return(
     <div class="header">
-      <input type="search" placeholder="search" id="head-search"/>
+     
       <img src="./banner1.png"/>
       </div>
       
