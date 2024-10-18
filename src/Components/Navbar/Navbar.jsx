af065b846369d404ff1e2cfe72ffc874999e6ffd
Sanjay-adepu <adepusanjay812@gmail.com>
2024-10-18 22:27:16 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -76,15 +76,20 @@ const Navbar = ({ onSearch, footerRef }) => {
       <Link id="new" to="/">
         <li>Home</li>
       </Link>
-      {!token ? (
+      
+     
+      <Link id="new" to="/cart">
+        <li>Cart</li>
+      </Link>
+
+ <li onClick={scrollToFooter}>Contact Us</li>
+
+{!token ? (
         <li onClick={() => setShowLogin(true)}>Sign In</li>
       ) : (
         <li onClick={handleLogout}>log out</li>
       )}
-      <li onClick={scrollToFooter}>Contact Us</li>
-      <Link id="new" to="/cart">
-        <li>Cart</li>
-      </Link>
+
     </ul>
   </div>
 )}
