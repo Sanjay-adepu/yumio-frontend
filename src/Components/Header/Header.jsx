c48ee0adf107a5dec77f06e489458c289b0de426
Sanjay-adepu <adepusanjay812@gmail.com>
2024-10-18 22:48:57 +0530

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,7 @@ const Header=()=>{
   
   return(
     <div class="header">
+  <marquee id="screen" > <h2> enable Desktop mode for better experience </h2> </marquee>
      
       <img src="./banner1.png"/>
       </div>
